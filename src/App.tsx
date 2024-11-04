152e26923cd9908b042ae413a2a335cdccfc3c63
Heberth López <heberth.lopez.19@gmail.com>
2024-11-04 14:27:46 -0500

    chore: :wrench: Added eslint + prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-center items-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className='flex flex-col items-center'>
+      <div className='flex justify-center items-center'>
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1 className="text-3xl">Vite + React</h1>
-      <div className="card">
+      <h1 className='text-3xl'>Vite + React</h1>
+      <div className='card'>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
