db20d5d5403f5601d1cef429c0e2aceb648bb2a2
Heberth López <heberth.lopez.19@gmail.com>
2024-11-10 00:20:36 -0500

    feat: :sparkles: Added button for back to Home in ProductDetail and improved styles

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { ChevronDownIcon, ShoppingCartIcon, Star } from 'lucide-react'
+import { Link, useParams } from 'react-router-dom'
+import {
+  ArrowBigLeftIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  Loader,
+  ShoppingCartIcon,
+  Star
+} from 'lucide-react'
 import Header from '@/components/Header'
 import { ProductData } from '@/types'
 
 export default function ProductDetail() {
   const { id } = useParams()
 
-  const [productData, setProductData] = useState<ProductData>({
-    options: { colors: [], storages: [] }
-  } as unknown as ProductData)
+  const [productData, setProductData] = useState<ProductData>({} as unknown as ProductData)
 
   useEffect(() => {
     fetch(`https://itx-frontend-test.onrender.com/api/product/${id}`)
@@ -17,6 +22,9 @@ export default function ProductDetail() {
       .then(data => setProductData(data))
       .catch(err => console.error(err))
   }, [id])
+
+  if (Object.keys(productData).length === 0)
+    return <Loader className='w-12 h-12 absolute top-1/2 left-1/2' />
 
   const { model, imgUrl, options } = productData
   const { colors, storages } = options
@@ -70,7 +78,7 @@ export default function ProductDetail() {
                 <h2 className='text-3xl font-bold'>{model}</h2>
                 <div className='flex items-center gap-2'>
                   <div className='flex items-center'>
-                    {[...Array(Math.round(Math.random() * 5))].map((_, i) => (
+                    {[...Array(Math.ceil(Math.random() * 5))].map((_, i) => (
                       <Star key={`star-${i}`} className='w-5 h-5 text-yellow-400 fill-current' />
                     ))}
                   </div>
@@ -82,15 +90,18 @@ export default function ProductDetail() {
               </div>
 
               <div className='space-y-4'>
-                <h3 className='text-lg font-semibold'>Description / Specs</h3>
+                <h3 className='text-lg font-semibold'>Description / Specs:</h3>
 
-                <ul className='list-disc list-inside space-y-1 text-gray-600 dark:text-indigo-100'>
+                <ul className='list-none list-inside space-y-1 text-gray-600 dark:text-indigo-100'>
                   {Object.entries(productData)
                     .filter(([key, _value]) => specsKeys.includes(key))
                     .map(([key, value]) => (
-                      <li key={key}>
-                        <span className='font-semibold'>{camelCaseToCapitalized(key)}: </span>
-                        {typeof value === 'string' ? value : value.join(' ')}
+                      <li key={key} className='flex items-center space-x-2'>
+                        <CheckIcon className='w-4 h-4 text-green-600 dark:text-green-300' />
+                        <p>
+                          <strong>{camelCaseToCapitalized(key)}: </strong>
+                          {typeof value === 'string' ? value : value.join(' ')}
+                        </p>
                       </li>
                     ))}
                 </ul>
@@ -102,7 +113,7 @@ export default function ProductDetail() {
                     Color
                   </label>
                   <div className='relative'>
-                    <select className='appearance-none dark:bg-indigo-900 px-4 py-2 w-full rounded-lg border-gray-300 shadow-sm focus:ring-2 focus:ring-indigo-500'>
+                    <select className='appearance-none bg-indigo-50 dark:bg-indigo-900 px-4 py-2 w-full rounded-lg border-gray-300 shadow-lg focus:ring-2 focus:ring-indigo-500'>
                       {colors.map(color => (
                         <option key={color.code} value={color.code}>
                           {color.name}
@@ -118,7 +129,7 @@ export default function ProductDetail() {
                     Storage
                   </label>
                   <div className='relative'>
-                    <select className='appearance-none dark:bg-indigo-900 p-2 w-full rounded-lg border-gray-300 shadow-sm focus:ring-2 focus:ring-indigo-500'>
+                    <select className='appearance-none bg-indigo-50 dark:bg-indigo-900 p-2 w-full rounded-lg border-gray-300 shadow-lg focus:ring-2 focus:ring-indigo-500'>
                       {storages.map(storage => (
                         <option key={storage.code} value={storage.code}>
                           {storage.name}
@@ -128,11 +139,21 @@ export default function ProductDetail() {
                     <ChevronDownIcon className='absolute right-2 top-1/2 -translate-y-1/2 h-5 w-5 text-indigo-500 dark:text-indigo-50 pointer-events-none' />
                   </div>
                 </div>
+
+                <div className='flex justify-around gap-2 pt-4'>
+                  <Link
+                    to='/'
+                    className='w-full max-w-48 justify-self-center py-3 px-5 bg-gradient-to-r from-indigo-600 to-blue-500 hover:bg-blue-700 hover:opacity-80 text-white font-medium rounded-lg flex items-center justify-center gap-2'
+                  >
+                    <ArrowBigLeftIcon className='w-4 h-4' />
+                    Volver al listado
+                  </Link>
+                  <button className='w-full max-w-48 justify-self-center py-3 px-5 bg-gradient-to-r from-indigo-600 to-blue-500 hover:bg-blue-700 hover:opacity-80 text-white font-medium rounded-lg flex items-center justify-center gap-2'>
+                    <ShoppingCartIcon className='w-4 h-4' />
+                    Agregar al carrito
+                  </button>
+                </div>
               </div>
-              <button className='w-max justify-self-center py-3 px-6 bg-gradient-to-r from-indigo-600 to-blue-500 hover:bg-blue-700 hover:opacity-80 text-white font-medium rounded-lg flex items-center justify-center gap-3'>
-                <ShoppingCartIcon className='w-5 h-5' />
-                Add to Cart
-              </button>
             </div>
           </div>
         </section>
